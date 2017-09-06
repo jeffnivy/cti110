bc6566853bc29e83d17301ff83abe1707eeca84a
jeffnivy <31700691+jeffnivy@users.noreply.github.com>
2017-09-06 15:47:30 -0400

    Second submission

---
+++ M2HW1_DistanceTraveled_JeffreyLee.py
@@ -0,0 +1,31 @@
+#CTI-110
+#M2HW1-Distance Traveled
+#Lee
+#9Sept2017
+#
+#Assuming there are no accidents or delays, the distance the car travels
+#down the interstate can be calculated with the following fomula
+# Distance = Speed * Time
+#A car is traveling at 70 miles per hour.
+#Write a program that displays the following:
+# The distance the car will travel in 6 hours
+# The distance the car will travel in 10 hours
+# The distance the car will travel in 15 hours
+
+# Assign a value to the speed variable.
+speed = 70
+
+# Assign a value to the time variable.
+time1 = 6
+time2 = 10
+time3 = 15
+
+# Get the distance traveled.
+distance1 = speed * time1
+distance2 = speed * time2
+distance3 = speed * time3
+
+print "Traveling at 70 miles per hour..."
+print "after 6 hours you will have traveled ", distance1, "miles."
+print "after 10 hours you will have traveled ", distance2, "miles."
+print "after 15 hours you will have traveled ", distance3, "miles."
