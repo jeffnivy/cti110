69afeb45fd64a8d5a54f8cedfb02542ea010db9b
jeffnivy <31700691+jeffnivy@users.noreply.github.com>
2017-12-11 10:50:19 -0500

    m5t1b

---
+++ M5T1B_initials_Lee.py
@@ -0,0 +1,35 @@
+#CTI 110
+#M5T1B drawing initials
+#Jeffrey Lee
+#17 Oct 2017
+
+#draw initials with turtle
+
+import turtle
+turtle.pencolor('green')
+turtle.pensize('10')
+
+t = turtle.pen()
+turtle.forward(75)
+turtle.backward(75)
+turtle.left(90)
+turtle.forward(20)
+turtle.backward(20)
+turtle.right(90)
+turtle.forward(75)
+turtle.left(90)
+turtle.forward(100)
+turtle.up()
+turtle.right(90)
+turtle.forward(20)
+turtle.down()
+turtle.right(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(75)
+
+
+
+
+
+
